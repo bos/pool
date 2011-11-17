995fcf9751d450ff13e4ea5f576a757fc5bb780a
Bryan O'Sullivan <bos@serpentine.com>
2011-11-17 13:26:23 -0800

    Update email addresses AGAIN

--- Data/Pool.hs
+++ Data/Pool.hs
@@ -4,7 +4,7 @@
 -- Module:      Data.Pool
 -- Copyright:   (c) 2011 MailRank, Inc.
 -- License:     BSD3
--- Maintainer:  Bryan O'Sullivan <bos@mailrank.com>
+-- Maintainer:  Bryan O'Sullivan <bos@serpentine.com>
 -- Stability:   experimental
 -- Portability: portable
 --
